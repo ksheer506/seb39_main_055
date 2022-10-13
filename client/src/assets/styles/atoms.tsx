809fba9b33b48f18488c7f522fbf953c1b947423
ksheer506 <ksheer506@naver.com>
2022-10-13 20:54:23 +0900

    feat: 자주 쓰이는 Style 모듈화

---
+++ client/src/assets/styles/atoms.tsx
@@ -0,0 +1,6 @@
+import { css } from "styled-components";
+
+export const formBorders = css`
+  border-radius: 10px;
+  box-shadow: 0px 0px 15px 0px #cfcfcf;
+`;
--- client/src/assets/styles/index.ts
+++ client/src/assets/styles/index.ts
@@ -1,2 +1,3 @@
+export * from "./atoms";
 export { default as GlobalStyle } from "./GlobalStyle";
 export * from "./Theme";
