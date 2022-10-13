6431c8409297e8354d5bd3c79b87085a9ae6d2b9
ksheer506 <ksheer506@naver.com>
2022-10-13 13:00:54 +0900

    style: 메인 화면 애니메이션 삭제

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/no-array-index-key */
-import { Fade } from "react-awesome-reveal";
 import styled from "styled-components";
 
 import {
@@ -16,35 +15,12 @@ import { menuList } from "../../constants";
 
 const Container = styled.div`
   min-height: inherit;
-  padding-top: 650px;
+  padding-top: 600px;
 
   & > section {
     display: flex;
     flex-direction: column;
-
-    & > div:nth-child(1) {
-      margin-bottom: 70px;
-    }
-
-    & > menu:nth-child(2) {
-      margin-bottom: 230px;
-    }
-
-    & > div:nth-child(3) {
-      margin-bottom: 300px;
-    }
-
-    & > div:nth-child(4) {
-      margin-bottom: 300px;
-    }
-
-    & > div:nth-child(5) {
-      margin-bottom: 300px;
-    }
-
-    & > div:nth-child(6) {
-      margin-bottom: 180px;
-    }
+    row-gap: 150px;
 
     @media screen and (max-width: ${({ theme }) => theme.breakPoints.tablet}) {
       & > div:nth-child(1) {
@@ -80,13 +56,10 @@ const Main = () => {
       <section>
         <SearchBar />
         <Category menuList={menuList} />
-        {[<Recommend />, <HotPlace />, <Pick />, <Review />].map(
-          (component, idx) => (
-            <Fade key={idx} direction="left">
-              {component}
-            </Fade>
-          )
-        )}
+        <Recommend />
+        <HotPlace />
+        <Pick />
+        <Review />
       </section>
     </Container>
   );
