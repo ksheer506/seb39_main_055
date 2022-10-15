64e7d7cf6a999077e98e266b166b050cdef363d0
ksheer506 <ksheer506@naver.com>
2022-10-15 20:53:37 +0900

    feat: 메인 메뉴 호버 효과 추가

--- client/src/assets/styles/atoms.tsx
+++ client/src/assets/styles/atoms.tsx
@@ -1,6 +1,29 @@
 import { css } from "styled-components";
 
+import { colors } from "./Theme";
+
 export const formBorders = css`
   border-radius: 10px;
   box-shadow: 0px 0px 15px 0px #cfcfcf;
+`;
+
+export const bottomHoverLine = css`
+  &::after {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    content: "";
+    bottom: 0px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 0px;
+    height: 3px;
+    margin-top: 3px;
+    background-color: ${colors("orange075")};
+    transition: 0.4s all;
+  }
+
+  &:hover::after {
+    width: 80%;
+  }
 `;
