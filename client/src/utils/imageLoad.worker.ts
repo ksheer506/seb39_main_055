c436eeb315fe17105ee22af66f823a6475569d5a
ksheer506 <ksheer506@naver.com>
2022-10-21 15:56:04 +0900

    refactor: Worker 함수 분리

--- client/src/utils/imageLoad.worker.ts
+++ client/src/utils/imageLoad.worker.ts
@@ -1,51 +1,80 @@
 /* eslint-disable no-restricted-globals */
 import { extractImageInfos } from "./imageProcessor";
 
-const MAX_WIDTH = 1000;
-const incompatible = ["svg+xml", "gif"];
-
 interface WorkerData {
   images: File[];
   canvas: any;
 }
 
+interface compressOptions {
+  maxSize?: number;
+  convertTo?: "image/png" | "image/webp";
+}
+
+function filterLargeImages(images: File[], maxMB: number) {
+  return images.filter((img) => {
+    const megabytes = img.size / (1000 * 1000);
+
+    if (megabytes >= maxMB) {
+      return false;
+    }
+
+    return true;
+  });
+}
+
+function downsizeDimensions(width: number, height: number, maxSize: number) {
+  const ratio = height / width;
+
+  if (width > height) {
+    const W = width > maxSize ? maxSize : width;
+    const H = W * ratio;
+
+    return [W, H];
+  }
+
+  const H = height > maxSize ? maxSize : height;
+  const W = H / ratio;
+
+  return [W, H];
+}
+
+function compressImages(
+  images: File[],
+  canvas: any,
+  options?: compressOptions
+) {
+  const ctx = canvas.getContext("2d");
+  const incompatible = ["svg+xml", "gif"];
+  const { maxSize = 1000, convertTo = "image/png" } = options || {};
+
+  return Promise.all(
+    images.map(async (img) => {
+      const extension = img.type.split("/")[1]; // createImageBitmap() 미지원 포맷 - SVG, GIF
+
+      if (incompatible.includes(extension)) return img;
+
+      const image = await createImageBitmap(img);
+      const { width, height } = image;
+      const [W, H] = downsizeDimensions(width, height, maxSize);
+
+      canvas.width = W;
+      canvas.height = H;
+      ctx.drawImage(image, 0, 0, W, H);
+
+      return canvas.convertToBlob({ quality: 0.75, type: convertTo });
+    })
+  );
+}
+
 self.addEventListener("message", async ({ data }: MessageEvent<WorkerData>) => {
   const { images, canvas } = data;
-  const ctx = canvas.getContext("2d");
-  let error = "";
+  const error = "";
 
-  const compressed = await Promise.all(
-    images
-      .filter((img) => {
-        const megabytes = img.size / (1000 * 1000);
-
-        if (megabytes > 10) {
-          error = "10MB 이상인 이미지는 업로드할 수 없습니다.";
-          return false;
-        }
-
-        return true;
-      })
-      .map(async (img) => {
-        const size = img.size / (1000 * 1000);
-        const extension = img.type.split("/")[1]; // createImageBitmap() 미지원 포맷 - SVG, GIF
-
-        if (incompatible.includes(extension) || size < 1) return img;
-
-        const image = await createImageBitmap(img);
-        const { width, height } = image;
-        const ratio = height / width;
-
-        const W = width > MAX_WIDTH ? MAX_WIDTH : width;
-        const H = W * ratio;
-
-        canvas.width = W;
-        canvas.height = H;
-        ctx.drawImage(image, 0, 0, W, H);
-
-        return canvas.convertToBlob({ quality: 0.8 });
-      })
-  );
+  const filtered = filterLargeImages(images, 10);
+  const compressed = await compressImages(filtered, canvas, {
+    convertTo: "image/webp",
+  });
 
   const processed = await extractImageInfos(compressed);
 
