0698f2b8968cf64c04cf3fcf59962fed8dece189
ksheer506 <ksheer506@naver.com>
2022-10-22 17:18:22 +0900

    #2 feat: EditPost에 updatedAt Props 추가

--- client/src/pages/Posts/EditPost/EditPost.tsx
+++ client/src/pages/Posts/EditPost/EditPost.tsx
@@ -6,31 +6,28 @@ import { ThreadPostResponse } from "../../../types";
 
 type ThreadPartialData = Pick<
   ThreadPostResponse,
-  "body" | "threadImages" | "threadId"
+  "body" | "threadImages" | "threadId" | "updatedAt"
 >;
 
 interface LocationWithState extends Location {
   state: ThreadPartialData;
 }
 
-function threadDataTransfromer({
-  body,
-  threadImages,
-  threadId,
-}: ThreadPartialData) {
-  const transformedImgs = threadImages.map(({ threadImage }) => ({
+function threadDataTransfromer(
+  threadImages: ThreadPostResponse["threadImages"]
+) {
+  return threadImages.map(({ threadImage }) => ({
     file: null,
     uri: threadImage,
     id: threadImage, // 서로 다른 이미지가 하나의 AWS url로 대응되지 않기 때문에 별도의 계산이 필요한 md5 대신 사용
   }));
-
-  return { body, threadImages: transformedImgs, threadId };
 }
 
 const EditPost = () => {
   // PostDetail에서 navigate()에서 state로 넘겨준 데이터 사용
   const { state } = useLocation() as LocationWithState;
-  const { body, threadImages, threadId } = threadDataTransfromer(state);
+  const { body, threadId, updatedAt } = state;
+  const threadImages = threadDataTransfromer(state.threadImages);
 
   return (
     <PostForms
@@ -39,6 +36,7 @@ const EditPost = () => {
       body={body}
       threadImages={threadImages}
       threadId={threadId}
+      updatedAt={updatedAt}
     />
   );
 };
