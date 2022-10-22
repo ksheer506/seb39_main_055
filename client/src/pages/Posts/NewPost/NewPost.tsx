7a04628ff54d315f2ec248c62b5d7ecfdb9f5515
ksheer506 <ksheer506@naver.com>
2022-10-22 19:50:33 +0900

    feat: 비로그인 상태일 때 로그인 페이지로 리다이렉트

--- client/src/pages/Posts/NewPost/NewPost.tsx
+++ client/src/pages/Posts/NewPost/NewPost.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 import { submitNewPost } from "../../../apis";
 import PostForms from "../../../components/PostForms/PostForms";
+import { selectUserInfos, useAppSelector } from "../../../redux";
 
 const NewPost = () => {
+  const { userId = -1 } = useAppSelector(selectUserInfos) || {};
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userId >= 0) return;
+
+    toast.warn("로그인해주세요.");
+    navigate("/login");
+  });
+
   return <PostForms buttonText="등록하기" mutation={submitNewPost} />;
 };
 
